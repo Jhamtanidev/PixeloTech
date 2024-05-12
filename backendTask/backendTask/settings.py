f5ddd264adb4db5e22673a39a3cf86a8495b4c59
Jhamtanidev <jhamtanidev2004@gmail.com>
2024-05-12 13:05:27 +0530

    deploy on vercel

--- backendTask/backendTask/settings.py
+++ backendTask/backendTask/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dj_database_url
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -84,9 +87,10 @@ DATABASES = {
         'PASSWORD': 'TGZqjsVZbNrNeMqNsteRUaEFdZDZXqDd',
         'HOST': 'viaduct.proxy.rlwy.net',
         'PORT': '27753',
+    
     }
 }
-
+DATABASES['default'] = dj_database_url.config()
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
