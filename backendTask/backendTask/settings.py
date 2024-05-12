34cbead1aeef7853265033cc176c53a04ffbf37b
Jhamtanidev <jhamtanidev2004@gmail.com>
2024-05-13 01:05:14 +0530

    updated settings.py

--- backendTask/backendTask/settings.py
+++ backendTask/backendTask/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['.vercel.app','.now.sh','127.0.0.1']
+ALLOWED_HOSTS = ['.vercel.app','.now.sh','127.0.0.1','https://swipepic.netlify.app/']
 
 
 # Application definition
